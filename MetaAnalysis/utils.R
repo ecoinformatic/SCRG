2dd10bcee95496b88beed216b5c4cfd68b9cb51f
Thomas Adriaan Hellinger <brotherjack1877@gmail.com>
2024-06-10 13:03:15 -0400

    added primative namespace for ease of lookup of SCRG utility functions

--- MetaAnalysis/utils.R
+++ MetaAnalysis/utils.R
@@ -1,22 +1,18 @@
 library(sf)
 library(stringdist)
 
-venn_set <- function(x, y) {
-  # Making a venn diagram of two vectors to determine what is not in the other 
-  # vector
-  # From: https://stackoverflow.com/a/17599164
-  both <- union(x,y)
-  inX <- both %in% x
-  inY <- both %in% y
-  return(table(inX,inY))
+scrg__get_features_from_sf <- function(sf) {
+  # This function takes a geospatial object and returns the features as a data
+  # frame.
+  return(as.data.frame(st_drop_geometry(sf)))
 }
 
-make_string_distance_diff <- function(x, y) {
+scrg__make_string_distance_diff <- function(x, y) {
   # Takes two character vectors and compares the string distances between each
   # element of both vectors. Function returns a matrix of the results.
   x <- sort(x)
   y <- sort(y)
-  if(venn_set(x, y)[4] == 0){
+  if(scrg__venn_set(x, y)[4] == 0){
     # No values in either vector match
     return(
       matrix(
@@ -46,8 +42,12 @@ make_string_distance_diff <- function(x, y) {
   }
 }
 
-get_features_from_sf <- function(sf) {
-  # This function takes a geospatial object and returns the features as a data
-  # frame.
-  return(as.data.frame(st_drop_geometry(sf)))
+scrg__venn_set <- function(x, y) {
+  # Making a venn diagram of two vectors to determine what is not in the other 
+  # vector
+  # From: https://stackoverflow.com/a/17599164
+  both <- union(x,y)
+  inX <- both %in% x
+  inY <- both %in% y
+  return(table(inX,inY))
 }
