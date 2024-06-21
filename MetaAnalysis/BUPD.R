9f47332e1d8af893b433e00768ddba7016ca77a1
George Zaragoza <george.zaragoza@gmail.com>
2024-06-21 19:20:04 -0400

    updating BUPD script and testing shapefile join with layer data

--- MetaAnalysis/BUPD.R
+++ MetaAnalysis/BUPD.R
@@ -1,13 +1,16 @@
 ############################################
-# HOUSEKEEPING
+# INITIALIZE
 ############################################
-data[predictors] <- lapply(data[predictors], as.factor) # convert to factor
-data[[response_var]] <- factor(data[[response_var]]) # convert to factor
+# Initial empty model
+# Note that `best_formula` starts with `initial_formula` as baseline
+initial_formula <- as.formula(paste(response_var, "~ 1"))
+best_formula <- initial_formula
+best_model <- multinom(best_formula, data = data, MaxNWts = 5000)
+best_aic <- AIC(best_model)
 name_prefix <- gsub(" ", "_", name) # add underscores
 
-
 ############################################
-# BUILD UP PHASE
+# BUILD-UP PHASE
 ############################################
 # Function to fit and evaluate models (similar to Chris' only with a multinomial logistic regression for this specific dataset)
 fit_and_evaluate <- function(formula, data) {
@@ -15,39 +18,42 @@ fit_and_evaluate <- function(formula, data) {
   AIC(model)  # Using AIC for simplicity, but you can choose other criteria
 }
 
-# Initial empty model
-# Note that `best_formula` starts with `initial_formula` as baseline
-initial_formula <- as.formula(paste(response_var, "~ 1"))
-best_formula <- initial_formula 
-best_model <- multinom(best_formula, data = data, MaxNWts = 5000)
-best_aic <- AIC(best_model)
+# Similar to Chris' only with a multinomial logistic regression (which can be switched out)
+for (i in 1:length(predictors)) {
+    current_predictors <- all.vars(best_formula)[-1]
+    remaining_predictors <- setdiff(predictors, current_predictors) 
+    candidate_models <- list()  # for storing model and their AIC
 
-#####
-# Build models iteratively
-for (i in 1:length(predictors)) { # begin outer loop by adding predictors one at a time
-  candidate_models <- list() # empty list to store candidate models and their AICs
-  for (j in i:length(predictors)) { # begine inner loop to test adding each predictor not included in current best model
-    new_formula <- update(best_formula, paste(". ~ . +", predictors[j]))
-    formula_str <- paste(deparse(new_formula, width.cutoff = 500), collapse = "") # converts model formulat to string. Note that the width.cutoff and collapse are EXTREMELY important or deparse will split your string into two lines by default
-    candidate_aic <- fit_and_evaluate(new_formula, data) # using the `fit_and_evaluate` function from above to fit new model and calculate AIC
-    candidate_models[[formula_str]] <- candidate_aic # store the new model and AIC in cadidate model list
-    print(paste("Testing build-up formula:", formula_str, "with AIC:", candidate_aic)) # helpful output
-  }
-  
-  # ID the best model
-  best_candidate <- which.min(sapply(candidate_models, identity)) # ID the model with lowest AIC among candidate model list
-  best_candidate_formula <- names(candidate_models)[best_candidate] # Get formula of best candidate model (string)
-  
-  # Compare with the current best model
-  if (candidate_models[[best_candidate_formula]] < best_aic) { # If the AIC of the best candidate model is lower than the current best model's AIC, it replaces the model with the new model
-    best_formula <- as.formula(best_candidate_formula) # Converts the model formula string back into a regular formula object
-    best_aic <- candidate_models[[best_candidate_formula]] # Updates best_aic to the AIC of the new best model
-    print(paste("New best model:", best_candidate_formula, "AIC:", best_aic))  # helpful output
-  } else {
-    print("No further improvement, stopping build-up.")
-    break  # Stop if the AIC of the best candidate model is not lower than the current best AIC
-  }
+    # Build models iteratively based on current best model
+    for (predictor in remaining_predictors) {
+        new_formula <- update(best_formula, paste(". ~ . +", predictor))
+        formula_str <- paste(deparse(new_formula, width.cutoff = 500), collapse = "") # converts model formulat to string. Note that the width.cutoff and collapse are EXTREMELY important or deparse will split your string into two lines by default
+        candidate_aic <- fit_and_evaluate(new_formula, data) # using the `fit_and_evaluate` function from above to fit new model and calculate AIC
+        candidate_models[[formula_str]] <- candidate_aic # store the new model and AIC in cadidate model list
+        print(paste("Testing build-up formula:", formula_str, "with AIC:", candidate_aic)) # helpful output
+    }
+
+    # ID the best model
+    if (length(candidate_models) > 0) {
+        best_candidate <- which.min(unlist(candidate_models)) # ID model with lowest AIC
+        best_candidate_formula <- names(candidate_models)[best_candidate] # Get formula of best candidate model (string)
+        best_candidate_aic <- unlist(candidate_models[best_candidate_formula])
+
+        # Update to the new best model if it improves AIC
+        if (best_candidate_aic < best_aic) { # If the AIC of the best candidate model is lower than the current best model's AIC, it replaces the model with the new model
+            best_formula <- as.formula(best_candidate_formula) # Converts the model formula string back into a regular formula object
+            best_aic <- best_candidate_aic # Updates best_aic to the AIC of the new best model
+            print(paste("New best model:", best_candidate_formula, "AIC:", best_aic)) # helpful output
+        } else {
+            print("No further improvement, stopping build-up.")
+            break  
+        }
+    } else {
+        print("No more predictors to test, stopping build-up.")
+        break
+    }
 }
+
 #####
 
 # Final model
@@ -109,7 +115,7 @@ coeff <- coef(final_model) # grab coefficients
 # confidence_intervals <- confint(final_model, level = 0.95) # Calculate CI
 odds_ratios <- exp(coeff) # Calculate OR
 
-######################
+###################### # Note: Need to fix paths or get better ones
 # `assign` to new variables based on name prefix chosen
 assign(paste0(name_prefix, "_final_model"), final_model)
 assign(paste0(name_prefix, "_final_form"), final_form)
